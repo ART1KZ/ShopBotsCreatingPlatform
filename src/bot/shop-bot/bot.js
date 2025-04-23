c9e9a0cb2a434e581c4ae1f3d51eb5bfe5a93acd
ART1KZ <artemkiselev18072k6@mail.ru>
2025-04-23 19:27:50 +0500

    fix import, start command

--- src/bot/shop-bot/bot.js
+++ src/bot/shop-bot/bot.js
@@ -1,10 +1,10 @@
 import { Bot } from "grammy";
-import { mainScene } from "./scenes/main/scene";
+import { mainScene } from "./scenes/main/scene.js";
 
 export function createShopBot(token) {
     const bot = new Bot(token);
 
-    bot.command("start", mainScene);
+    bot.command("start", async (ctx) => await mainScene(ctx));
 
     return bot;
 }
