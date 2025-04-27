b4587003ca2cb721553253cd84e6524357f6dfbb
Probe1st <svintsitskiy.leha@mail.ru>
2025-04-27 05:21:41 +0500

    fix index.js in ./src

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import { bot as mainBot } from "./bot/main-bot/bot.js";
 import { startAllActiveBots, startShopBot } from "./bot/shared/utils/shopBotsManager.js";
 
 mainBot.start();
-// startAllActiveBots();
-startShopBot("7702384595:AAEky5pv98YYFZ0PWB9oijHQI73wEq2nJXg");
-
+startAllActiveBots();
